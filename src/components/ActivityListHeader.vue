a2f350f2ceb53db0c059bd7017dfc6bf934180a0
XingYing99 <71431944+XingYing99@users.noreply.github.com>
2020-10-13 17:55:50 +0800

    added weather details in activitylist header

--- src/components/ActivityListHeader.vue
+++ src/components/ActivityListHeader.vue
@@ -1,28 +1,79 @@
+
 <template>
    <div>
-     <header>
+     <header style="background-image:url(../assets/Rain.jpg);">
         <h2>Trip <br> Details</h2>
         <h3 id="title1">Selected Date</h3>
         <p>{{date}} </p>
         <h3 id= "title2">Weather</h3>
+              <h3>{{Selected_weather}}</h3>
+              <img v-bind:src="icon"/>
+
 
       </header> 
     </div>
 </template>
 
+
 <script>
+import axios from 'axios'
 
 export default {
 
   data(){
     return{
-      date:''
+      date:'',
+      weather_results: [],
+      Selected_weather:'',
+      icon:'',
+      picture:''
+      
+
+      
+
     
     }
   },
+  methods:{
+    
+    fetchData : function(){
+        axios.get('https://api.weatherbit.io/v2.0/forecast/daily?city=Singapore&key=782b9a2558104ec1898b21424a5780b4').then(response=>{
+        this.weather_results=response.data.data
+        console.log(this.weather_results)
+        for(let key in this.weather_results){
+          console.log(this.date)
+          console.log(this.weather_results[key].datetime)
+          if(this.weather_results[key].datetime==this.date ){
+            this.Selected_weather=this.weather_results[key].weather.description
+            console.log(this.Selected_weather)
+            this.icon="https://www.weatherbit.io/static/img/icons/"+this.weather_results[key].weather.icon+".png"
+            
+          }
+            
+        }
+    }).catch(error => console.log(error))
+    
+    },
+    picture_display:function(){
+      if(this.Selected_weather.includes("rain")){
+        return "../assets/Rain.jpg";
+      }else{
+        return "../assets/Cloud.jpg";
+
+      }
+
+
+    }
+    
+    },
+
+
+
   mounted() {
     if (sessionStorage.date) {
       this.date = sessionStorage.date;
+      this.fetchData();
+      
     }
   }
   
@@ -32,7 +83,7 @@ export default {
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 header{
-    background-image: url(../assets/Rain.jpg);
+    
     background-repeat: no-repeat;
     overflow: hidden;
     background-size: cover;
@@ -49,22 +100,42 @@ h2{
 
 }
 h3{
-    padding:5px;
-    color : rgb(112, 112, 112);
+    
+    
     float: left;
-    margin-left: 80%;
+    margin-left: 75%;
     line-height: 0px;
+    color: ivory;
+    margin-top: 2%;
+    text-size-adjust: 8px;
+ 
 }
 p{
   position: absolute;
-  left:89%;
-  top:5%;
+  left:85.3%;
+  top:4%;
   color:white;
   font-weight:bold;
 }
 #title2{
   position: relative;
   margin-top: 3%;
+   color : rgb(112, 112, 112);
+   font-display: bold;
+   font-display:Abel;
 }
+#title1{
+  position: relative;
+  margin-top: 1%;
+  color : rgb(112, 112, 112);
+}
+img{
+  float:right;
+  width:80px;
+  height: 80px;
+  margin-right:8%;
+
+}
+
 
 </style>
